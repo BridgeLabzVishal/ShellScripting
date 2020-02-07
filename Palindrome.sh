c8e1bd8324b5466c9878001c4b1ee57731fc55f0
RedDevilVishal <yadav.vishal1611@gmail.com>
2020-02-07 19:09:33 +0530

    [Vishal Yadav]add. Implemented Palindrome Number

---
+++ Palindrome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Program to check whether number is palindrome or not.
+
+echo "Enter a number : "
+read number
+
+Temp=$number
+Reverse=""
+while [ $Temp -gt 0 ]
+do
+   Remainder=$(expr $Temp % 10) 
+   Temp=$(expr $Temp / 10)
+   Reverse=$( echo ${Reverse}${Remainder} )
+done
+if [ $number -eq $Reverse ]
+then 
+   echo "Number is Palindrome"
+else
+   echo "Number is not Palindrome"
+fi
